d3f51bdab9fb36c721051e9d3d808c2980c2354f
venkatesh <morlavenkatesh19@gmail.com>
2026-03-15 19:20:27 +0530

    added re

--- apps/checkmate-apis/src/app/app.service.ts
+++ apps/checkmate-apis/src/app/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import {sampleText} from '@vittav-repos/utils';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: sampleText() };
+    return { message: "hello world" };
   }
 }
